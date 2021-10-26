0cf3f48df4284c89f1e639765c525e72b41c54b4
Ibrahim.H <bitsnaps@yahoo.fr>
2021-10-26 09:46:15 +0100

    update a dependency

--- config/db.php
+++ config/db.php
@@ -2,7 +2,7 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=tessnimdb',
+    'dsn' => 'mysql:host=localhost;dbname=eqraadb',
     'username' => 'root',
     'password' => '',
     'charset' => 'utf8',
